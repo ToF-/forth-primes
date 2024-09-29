dd0bb99a6f5944be447045e9e03a6db2f45c2b06
Christophe Thibaut <cthibauttof@gmail.com>
2024-09-29 18:08:02 +0200

    chapter 2

--- 168-primes.fs
+++ 168-primes.fs
@@ -1,6 +1,3 @@
-\ p-trial.fs   compute primes by trial division algorithm, using prime divisors
-
-CREATE SMALL-PRIMES
    2 ,   3 ,   5 ,   7 ,  11 ,  13 ,  17 ,  19 ,  23 ,  29 ,  31 ,  37 ,  41 ,  43 ,
   47 ,  53 ,  59 ,  61 ,  67 ,  71 ,  73 ,  79 ,  83 ,  89 ,  97 , 101 , 103 , 107 ,
  109 , 113 , 127 , 131 , 137 , 139 , 149 , 151 , 157 , 163 , 167 , 173 , 179 , 181 ,
@@ -14,45 +11,3 @@ CREATE SMALL-PRIMES
  811 , 821 , 823 , 827 , 829 , 839 , 853 , 857 , 859 , 863 , 877 , 881 , 883 , 887 ,
  907 , 911 , 919 , 929 , 937 , 941 , 947 , 953 , 967 , 971 , 977 , 983 , 991 , 997 ,
 
-168 CONSTANT SMALL-PRIMES-MAX
-1000 CONSTANT SMALL-PRIME-LIMIT
-
-: NTH-PRIME ( n -- p )
-    CELLS SMALL-PRIMES + @ ;
-
-: SQUARED ( n -- n² )
-    DUP * ;
-
-: IS-MULTIPLE? ( m,n -- f )
-    MOD 0= ;
-
-SMALL-PRIME-LIMIT SQUARED CONSTANT PRIME-LIMIT
-
-: IS-PRIME? ( n -- f )
-    ASSERT( DUP PRIME-LIMIT <= )
-    TRUE SWAP
-    SMALL-PRIMES-MAX 0 DO
-        I NTH-PRIME
-        2DUP SQUARED < IF
-            DROP LEAVE
-        ELSE
-            OVER SWAP IS-MULTIPLE? IF
-                NIP FALSE SWAP LEAVE
-            THEN
-        THEN
-    LOOP DROP ;
-
-: .PRIMES ( n -- )
-    1+ 2 DO
-        I IS-PRIME? IF
-            I . CR
-        THEN
-   LOOP ;
-
-: PRIME-COUNT ( n -- t )
-    0 SWAP
-    1+ 2 DO
-        I IS-PRIME? IF
-            1+
-        THEN
-    LOOP ;
--- print-init-168-primes.fs
+++ print-init-168-primes.fs
@@ -1,12 +1,16 @@
+\ print-init-168-primes.fs output code for prime divisor table init
 REQUIRE trial.fs
 
 : .PRIME-TABLE
     0 1000 2 DO
         I IS-PRIME? IF
-            I 4 .R ." ,"
+            I 4 .R ."  ,"
             1+
             DUP 14 MOD 0= IF
                 CR
             THEN
         THEN
     LOOP DROP CR ;
+
+.PRIME-TABLE
+BYE
